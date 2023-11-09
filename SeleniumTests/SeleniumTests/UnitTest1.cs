a72c9abfe7f2677c45ff1447d4a3357ce816f9ec
Roman Orlov <helltester666@gmail.com>
2023-11-09 23:18:40 +0100

    initial

---
+++ SeleniumTests/SeleniumTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumTests;
+
+public class Tests
+{
+    private IWebDriver _driver;
+    
+    [SetUp]
+    public void Setup()
+    {
+        // Chrome
+        // new DriverManager().SetUpDriver(new ChromeConfig());
+        // _driver = new ChromeDriver();
+        
+        // Firefox
+        // new DriverManager().SetUpDriver(new FirefoxConfig());
+        // _driver = new FirefoxDriver();
+        
+        // Edge
+        // new DriverManager().SetUpDriver(new EdgeConfig());
+        // _driver = new EdgeDriver();
+        
+        _driver.Navigate().GoToUrl("https://orlovrs.github.io/time-tracker/");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+    }
+    
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
